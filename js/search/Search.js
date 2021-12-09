b9fabe29ae0c5a4a118c7dd3c36dc20fbca38268
CL <cl@gmail.com>
2021-12-09 01:32:49 +0100

    boucle native for

--- js/search/Search.js
+++ js/search/Search.js
@@ -4,15 +4,20 @@ export default class Search {
 	static searchMainInput(value) {
 		let recipesMatched = [];
 
-		recipes.forEach((recipe) => {
-			if (
-				recipe.name.includes(value) ||
-				recipe.description.includes(value) ||
-				recipe.ingredients.some((elt) => elt.ingredient.includes(value))
-			) {
-				recipesMatched.push(recipe);
+		for (let i = 0; i < recipes.length; i++) {
+			const { name, ingredients, description } = recipes[i];
+			const includesInName = name.includes(value);
+			const includesInDescription = description.includes(value);
+			let includesInIngredients = false;
+			for (let y = 0; y < ingredients.length; y++) {
+				if (ingredients[y].ingredient.includes(value)) {
+					includesInIngredients = true;
+				}
 			}
-		});
+			if (includesInName || includesInDescription || includesInIngredients) {
+				recipesMatched.push(recipes[i]);
+			}
+		}
 		return {
 			recipesMatched,
 		};
